d163ebe5282aedc76a24a8eb7afc634ab0bf4cfc
Onaroc <33450785+Onaroc@users.noreply.github.com>
2017-11-20 13:06:35 +0000

    Add files via upload

--- lab8.py
+++ lab8.py
@@ -4,6 +4,11 @@ Created on Mon Nov 13 12:11:06 2017
 @author: JFord
 """
 import math
+import matplotlib.pyplot as plt
+
+
+def f(x):
+    return x * x
 
 
 def f1(x):
@@ -19,7 +24,7 @@ def f2(x):
     Accepts a number x as an input and returns
     log (x + 2.1)
     """
-    return math.log(x + 2.1)
+    return math.log(x + 2.2)
 
 
 def positive_places(f, xs):
@@ -44,7 +49,20 @@ def create_plot_data(f, xmin, xmax, n):
     for i in ival:
         xi = xmin + i * ((xmax - xmin) / (n - 1))
         xs.append(xi)
-    xy = [f(x) for x in xs]
-    return (xs, xy)
+    ys = [f(x) for x in xs]
+    return (xs, ys)
 
 
+def my_plot():
+    """
+    Computes and plots f1(x) and f2(x) (using create_plot_data)
+    on a graph between -2 and 2 for 1001 points, then
+    returns none
+    """
+    dataf1 = (create_plot_data(f1, -2, 2, 1001))
+    dataf2 = (create_plot_data(f2, -2, 2, 1001))   
+    plt.plot(dataf1)
+    plt.plot(dataf2)
+    plt.xlabel('f(x)')
+    plt.show
+    return None
