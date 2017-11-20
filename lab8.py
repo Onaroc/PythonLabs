cfe3e9622090ca776cfb5cfed4fc450f21a4f2c4
Onaroc <33450785+Onaroc@users.noreply.github.com>
2017-11-20 12:06:57 +0000

    Add files via upload

--- lab8.py
+++ lab8.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Nov 13 12:11:06 2017
-
 @author: JFord
 """
 import math
@@ -23,16 +22,6 @@ def f2(x):
     return math.log(x + 2.1)
 
 
-def pos(x):
-    """
-    checks if x is larger than 0
-    """
-    if x > 0:
-        return True
-    else:
-        return False
-
-
 def positive_places(f, xs):
     """
     Takes as an argument a function f and a list
@@ -40,4 +29,22 @@ def positive_places(f, xs):
     only those elemnts x of xs for which f(x) is
     strictly greater than zero
     """
-    
+    lst = [f(x) for x in xs if (f(x) > 0)]
+    return lst
+
+
+def create_plot_data(f, xmin, xmax, n):
+    """
+    Returns a tuple (xs, ys)
+    where xs and ys are two sequences
+    each containing n numbers
+    """
+    xs = []
+    ival = range(n)
+    for i in ival:
+        xi = xmin + i * ((xmax - xmin) / (n - 1))
+        xs.append(xi)
+    xy = [f(x) for x in xs]
+    return (xs, xy)
+
+
