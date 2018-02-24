580953570e9dd52226b0c52e03c07ca1f55bb3f6
Onaroc <33450785+Onaroc@users.noreply.github.com>
2018-02-24 12:31:21 +0000

    Update and rename  lab1.py to lab1.py

--- Semester 1/lab1.py
+++ Semester 1/lab1.py
@@ -9,6 +9,8 @@ def average(a, b):
     """Given parameters a and b, compute and
        return the arithmetic mean of a and b"""
     return (a + b) * 0.5
+
+
 def distance(a, b):
     """Given parameters a and b, compute and
        return the arithmetic mean of a and b"""
@@ -16,12 +18,16 @@ def distance(a, b):
        return (a - b)
     else:
        return -(a - b)
+
+
 def geometric_mean(a, b):
     """returns the geometric mean of two numbers
        i.e. the edge length a square would have to
        have so that its area equals that of a
        rectangle with sides a and b"""
     return (a * b) ** 0.5
+
+
 def pyramid_volume(A, h):
     """computes and returns the volume of a
        pyramid with base area A and height h"""
